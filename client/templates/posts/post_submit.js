f88407bf4fd4c4412859b80659519ddd1bfaf6e3
monkback <monkback@gmail.com>
2016-03-22 20:18:03 +0800

    microscope

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,34 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return throwError(error.reason);
+
+      // show this result but route anyway
+      if (result.postExists)
+        throwError('this link has already been posted');
+
+      Router.go('postPage', {_id: result._id});  
+    });
+  }
+});
